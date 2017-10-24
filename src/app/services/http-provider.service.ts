67fa701823f31ab6ec783cf7e24d567422857b36
enterpi <shivakumar00017@gmail.com>
2017-10-24 17:55:30 +0530

    cache http service

--- src/app/services/http-provider.service.ts
+++ src/app/services/http-provider.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse} from '@angular/common/http';
 
 import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class HttpProviderService implements HttpInterceptor {
 
+  private cache = {};
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const dup = req.clone({setHeaders: {'Content-Type': 'application/json'}});
-    return next.handle(dup);
+
+    if (req.method !== 'GET') {
+      return next.handle(req);
+    }
+
+    const cachedResponse = this.cache[req.urlWithParams] || null;
+    if (cachedResponse) {
+      return Observable.of(cachedResponse);
+    }
+
+    return next.handle(req).do(event => {
+      if (event instanceof HttpResponse) {
+      	this.cache[req.urlWithParams] = event;
+      }
+    });
+
+    //return next.handle(dup);
   }
 
 }
